078ae018580a6af844bd6c414c439b33c0e1c2a5
Nithish06 <nithss0404@gmail.com>
2020-10-05 21:53:23 +0530

    first commit

---
+++ build/precache-manifest.b13f06c628c1501a53abf5410a9f5543.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ffd79f242f8b2a0e52ad8ac5c78d76d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d07a2e7480a94e5482e3",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "a88a397c93486327bb51",
+    "url": "/static/css/main.a65ec1d2.chunk.css"
+  },
+  {
+    "revision": "d07a2e7480a94e5482e3",
+    "url": "/static/js/2.5336f457.chunk.js"
+  },
+  {
+    "revision": "af5492f10234ec8ba29a624c4fe6fb11",
+    "url": "/static/js/2.5336f457.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a88a397c93486327bb51",
+    "url": "/static/js/main.bdd02d80.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
